c5a10f428e073152495c736d4284be4c3d6431c6
Ernest Ruzindana <ruzindana.ernest99@gmail.com>
2022-07-21 21:20:21 +0200

    the last change

--- src/middlewares/contactValidation.js
+++ src/middlewares/contactValidation.js
@@ -5,10 +5,13 @@ const contactValidationSchema = joi.object({
     names: joi.string().required().min(2).regex(/^[A-Za-z]+$/).messages({
         "string.pattern.base": "The name field can not include numbers and special characters"
     }),
+
     email: joi.string().required().email(),
+
     phoneNumber: joi.string().regex(/^([+]\d{2})?\d{10}$/).required().messages({
         "string.pattern.base": "Invalid phone number"
     }),
+    
     message: joi.string().required()
 })
 
